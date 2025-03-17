50d3b0d1f4bb7a70be652f79fc1affc4a9d60204
Aryan Chaturvedi <aryanchaturvedi075@gmail.com>
2025-03-17 09:24:20 -0400

    Added View Result button and End Game Report Scene

---
+++ Assets/Scripts/GameNavigation/Navigation.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Navigation : MonoBehaviour
+{
+    public void ViewResults()
+    {
+        SceneManager.LoadScene("MidGameProgress");
+    }
+}
