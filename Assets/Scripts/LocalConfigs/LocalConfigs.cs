6e2aceec87e420cf82e087c0f937118212f93693
Danlin Luo <danlinluo432@gmail.com>
2025-04-06 23:36:28 -0400

    Local configs navigation

---
+++ Assets/Scripts/LocalConfigs/LocalConfigs.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+ using UnityEngine.SceneManagement;
+ 
+ public class LocalConfigs : MonoBehaviour
+ {
+ 
+     public void StartGame()
+     {
+         SceneManager.LoadScene("TerrainMars");
+     }
+ }
